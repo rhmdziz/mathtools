2db0f37f519283695158fb2df353087ca27f778b
Aziz Rahmad Isnanto <67959171+rhmdziz@users.noreply.github.com>
2021-03-14 10:17:49 +0700

    Update mencari-x-spldv-abc.py

--- mencari-x-spldv-abc.py
+++ mencari-x-spldv-abc.py
@@ -2,14 +2,10 @@ import math
 while True:
   def cari(a, b, c):
     d = math.sqrt(b**2 - 4*a*c)
-    x1 = int((-b - d) / (2 * a))
-    x2 = int((-b + d) / (2 * a))
-    return x1, x2
-
+    x1 = (-b - d) / (2 * a)
+    x2 = (-b + d) / (2 * a)
+    print(x1, x2)
   a = int(input("a: "))
   b = int(input("b: "))
   c = int(input("c: "))
-
-  x1, x2 = cari(a, b, c)
-  print("x1=",x1)
-  print("x2=",x2,"\n")
+  cari(a, b, c)
