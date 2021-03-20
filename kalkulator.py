b01ab03290556b1367b9ddd192f746f370aea9ab
Aziz Rahmad Isnanto <67959171+rhmdziz@users.noreply.github.com>
2021-03-20 20:27:24 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -5,88 +5,88 @@ def kalkulator():
         a = int(input())
         b = int(input())
         print(a+b)
-    def kurang():
-        print('Pengurangan')
-        a = int(input())
-        b = int(input())
-        print(a-b)
-    def kali():
-        print('Perkalian')
-        a = int(input())
-        b = int(input())
-        print(a*b)
-    def bagi():
-        print('Pembagian')
-        a = int(input())
-        b = int(input())
-        print(a/b)
-    def pangkat():
-        print('Perpangkatan')
-        a = int(input())
-        b = int(input())
-        print(a**b)
-    def akar2():
-        print('Pengakar2an')
-        a = int(input())
-        a = math.sqrt(a)
-        print(a)
-    def modulo():
-        print('Pencarian sisa bagi')
-        a = int(input())
-        b = int(input())
-        print(a%b)
-    print('Pilih operasi')
-    print('1. Jumlah       (+)\n2. Kurang       (-)\n3. Kali         (*)\n4. Bagi         (/)\n5. Pangkat      (**)\n6. Akar kuadrat (sqrt)\n7. Modulo       (%)')
-    ops=int(input('1/2/3/4/5/6/7: '))
-    if ops == 1:
-        jumlah()
         loop = input('lagi? y/n: ')
         if loop == 'y' or loop == 'Y':
             jumlah()
         else:
             kalkulator()
-    elif ops == 2:
-        kurang()
+    def kurang():
+        print('Pengurangan')
+        a = int(input())
+        b = int(input())
+        print(a-b)
         loop = input('lagi? y/n: ')
         if loop == 'y' or loop == 'Y':
             kurang()
         else:
             kalkulator()
-    elif ops == 3:
-        kali()
+    def kali():
+        print('Perkalian')
+        a = int(input())
+        b = int(input())
+        print(a*b)
         loop = input('lagi? y/n: ')
         if loop == 'y' or loop == 'Y':
             kali()
         else:
             kalkulator()
-    elif ops == 4:
-        bagi()
+    def bagi():
+        print('Pembagian')
+        a = int(input())
+        b = int(input())
+        print(a/b)
         loop = input('lagi? y/n: ')
         if loop == 'y' or loop == 'Y':
             bagi()
         else:
             kalkulator()
-    elif ops == 5:
-        pangkat()
+    def pangkat():
+        print('Perpangkatan')
+        a = int(input())
+        b = int(input())
+        print(a**b)
         loop = input('lagi? y/n: ')
         if loop == 'y' or loop == 'Y':
             pangkat()
         else:
             kalkulator()
-    elif ops == 6:
-        akar2()
+    def akar2():
+        print('Pengakar2an')
+        a = int(input())
+        a = math.sqrt(a)
+        print(a)
         loop = input('lagi? y/n: ')
         if loop == 'y' or loop == 'Y':
             akar()
         else:
             kalkulator()
-    elif ops == 7:
-        modulo()
+    def modulo():
+        print('Pencarian sisa bagi')
+        a = int(input())
+        b = int(input())
+        print(a%b)
         loop = input('lagi? y/n: ')
         if loop == 'y' or loop == 'Y':
             modulo()
         else:
             kalkulator()
+    print('Pilih operasi')
+    print('1. Jumlah       (+)\n2. Kurang       (-)\n3. Kali         (*)\n4. Bagi         (/)\n5. Pangkat      (**)\n6. Akar kuadrat (sqrt)\n7. Modulo       (%)')
+    ops=int(input('1/2/3/4/5/6/7: '))
+    if ops == 1:
+        jumlah()
+    elif ops == 2:
+        kurang()
+    elif ops == 3:
+        kali()
+    elif ops == 4:
+        bagi()  
+    elif ops == 5:
+        pangkat() 
+    elif ops == 6:
+        akar2()
+    elif ops == 7:
+        modulo()   
     else:
        kalkulator()
 kalkulator()
