dfa1247ba9035a602c8dc3d3af71bcd4558d7cbd
Aziz Rahmad Isnanto <67959171+rhmdziz@users.noreply.github.com>
2021-03-26 11:12:11 +0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,8 @@
-while True:
-  a = int(input('a: '))
-  b = int(input('u2: '))
-  d = int(input('un: '))
-  for i in range(d+1)
-    print(a, b, end=' ')
-    c = a + b
-    a, b = b, c
-    print(c, end=' ')
+a = int(input('a : '))
+b = int(input('u2: '))
+d = int(input('un: '))
+print(a, b, end=' ')
+for i in range(d-2):
+  c = a + b
+  a, b = b, c
+  print(c, end=' ')
